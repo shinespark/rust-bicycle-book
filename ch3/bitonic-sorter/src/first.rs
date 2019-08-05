c527e75b90a90e871e0c341e5346c1f86308f946
shinespark <shnsprk@gmail.com>
2019-08-06 05:05:09 +0900

    sort関数実装

--- ch3/bitonic-sorter/src/first.rs
+++ ch3/bitonic-sorter/src/first.rs
@@ -1,6 +1,11 @@
 pub fn sort(x: &mut [u64], up: bool) {
-    // 未実装
-    unimplemented!();
+    if x.len() > 1 {
+        let mid_point = x.len() /2;
+        sort(&mut x[..mid_point], true);
+        sort(&mut x[mid_point..], false);
+        sub_sort(x, up);
+    }
+
 }
 
 fn sub_sort(x: &mut [u64], up:bool) {
